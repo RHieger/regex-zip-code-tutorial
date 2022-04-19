a06dc806b885703a56eeb00b2d1397215fa8233c
Robert Hieger <rhieger@gmail.com>
2022-04-19 01:44:34 -0400

    Append text nodes to match paragraphs

--- js/app.js
+++ js/app.js
@@ -35,13 +35,11 @@
  10022-3337
  eNuhfF!.`;
 
- /* FOR DEBUGGING PURPOSES ONLY */
-
-//const testString = '10003 asdf10003 10003-8924 10003asdf 20012 60661 94015-9109 94015-9109asdf';
-
-const regex = /^[^A-Za-z][0-9]{5}(-[0-9]{4})?/gm;
+ const regex = /^[^A-Za-z][0-9]{5}(-[0-9]{4})?/gm;
 
 const matches = testString.match(regex);
+
+console.log(matches);
 
 let count = 0;
 
@@ -61,14 +59,50 @@ const paraCount = matches.length; // Number of paragraphs needed for matches
 
 console.log(`Number of Match paragraphs: ${paraCount}`);  // DEBUG PURPOSES ONLY
 
-// Build DOM nodes to list Regex Matches
+// Append domNodes to resultBox.
 
-const newPara = document.createElement('p');
+const domNode1 = document.createElement('p');
+const domNode2 = document.createElement('p');
+const domNode3 = document.createElement('p');
+const domNode4 = document.createElement('p');
+const domNode5 = document.createElement('p');
+const domNode6 = document.createElement('p');
+const domNode7 = document.createElement('p');
+const domNode8 = document.createElement('p');
+const domNode9 = document.createElement('p');
+const domNode10 = document.createElement('p');
+const domNode11 = document.createElement('p');
 
-const domNodes = [];
+const addParagraphs = () => {
+  resultBox.appendChild(domNode1);
+  resultBox.appendChild(domNode2);
+  resultBox.appendChild(domNode3);
+  resultBox.appendChild(domNode4);
+  resultBox.appendChild(domNode5);
+  resultBox.appendChild(domNode6);
+  resultBox.appendChild(domNode7);
+  resultBox.appendChild(domNode8);
+  resultBox.appendChild(domNode9);
+  resultBox.appendChild(domNode10);
+  resultBox.appendChild(domNode11);
+};
 
-for (count = 0; count < matches.length; count++) {
-  domNodes.push(newPara);
-}
+const populateResultBox = () => {
+  domNode1.textContent = ' Match #1: ' + matches[0];
+  domNode2.textContent = ' Match #2: ' + matches[1];
+  domNode3.textContent = ' Match #3: ' + matches[2];
+  domNode4.textContent = ' Match #4: ' + matches[3];
+  domNode5.textContent = ' Match #5: ' + matches[4];
+  domNode6.textContent = ' Match #6: ' + matches[5];
+  domNode7.textContent = ' Match #7: ' + matches[6];
+  domNode8.textContent = ' Match #8: ' + matches[7];
+  domNode9.textContent = ' Match #9: ' + matches[8];
+  domNode10.textContent = 'Match #10: ' + matches[9];
+  domNode11.textContent = 'Match #11: ' + matches[10];
+};
 
-console.log(domNodes);  // DEBUG ONLY
+addParagraphs();
+
+populateResultBox();
+
+console.log(resultBox); // DEBUG ONLY
