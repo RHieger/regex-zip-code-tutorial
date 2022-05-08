f3f38a67bfb5c9e82e6fc5cdf6e1479d594b4ace
Robert Hieger <rhieger@gmail.com>
2022-05-08 17:52:37 -0400

    Make minor code formatting correction

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@ const regex = /^[0-9]{5}(-[0-9]{4})?$/gm;
 
 const matches = testString.match(regex);
 
+
 /** The algorithm below is adapted freely from a YouTube
  *  tutorial at this URL:
  *  https://www.youtube.com/watch?v=1s4s_lU83pM
